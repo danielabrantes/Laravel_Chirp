f5748a8a3609e2dba4abf195cc395fe93d6501d3
pedro <daniel.abrantes@gmail.com>
2024-08-27 21:47:33 +0100

    Build Chirper with Blade

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Chirp extends Model
 {
     use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
